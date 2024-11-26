53ba064493126e8d93bf50d0883355f68a649fa3
Davide Primino <dprimino@olivelliputelli.it>
2024-11-26 10:40:04 +0100

    Componente Dadi Classe Dado

---
+++ INT4.ClassLibrary/Dado.cs
@@ -0,0 +1,14 @@
+﻿
+namespace INT4.ClassLibrary
+{
+    public class Dado
+    {
+        public int Faccia { get; private set; }
+        public bool IsBlocked { get; set; } = false;
+        public Dado() { TiraDado(); }
+        public void TiraDado() {
+            if (IsBlocked) return;
+            Faccia = Random.Shared.Next(1, 7);
+        }
+    }
+}
