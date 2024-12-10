adbbfc9e4dc1153de0664a0c2107bde10168edf3
Davide Primino <dprimino@olivelliputelli.it>
2024-12-10 11:27:49 +0100

    Persone

--- INT4.ClassLibrary/Persona.cs
+++ INT4.ClassLibrary/Persona.cs
@@ -1,20 +1,5 @@
 ﻿
 namespace INT4.ClassLibrary
 {
-    public class Persona
-    {
-        public string Nome { get; set; } = string.Empty;
-        public DateOnly DataDiNascita { get; set; }
-        public string Nazionalita { get; set; } = "Italia";
-        public int Eta()
-        {
-            DateTime oggi = DateTime.Today;
-            int eta = oggi.Year - DataDiNascita.Year;
-            if (oggi.Month < DataDiNascita.Month || (oggi.Month == DataDiNascita.Month && oggi.Day < DataDiNascita.Day))
-            {
-                eta--;
-            }
-            return eta;
-        }
-    }
+ 
 }
--- INT4.ClassLibrary/Studente.cs
+++ INT4.ClassLibrary/Studente.cs
@@ -1,11 +1,6 @@
 ﻿
 namespace INT4.ClassLibrary
 {
-    public class Studente : Persona {
-        public string ScuolaFrequentata { get; set; } = string.Empty;
-    }
-
-    public class StudenteUniversitario : Studente { }
 
 
 }
