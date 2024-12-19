e1f44df8b6cd4cec66a9e76b3b67b3c831a34e30
Davide Primino <dprimino@olivelliputelli.it>
2024-12-19 09:50:46 +0100

    Pagina Todo

--- INT4.ClassLibrary/TodoItem.cs
+++ INT4.ClassLibrary/TodoItem.cs
@@ -1,8 +1,10 @@
-﻿namespace INT4.ClassLibrary
+﻿namespace INT4.ClassLibrary;
+public class TodoItem
 {
-    public class TodoItem
-    {
-        public string? Title { get; set; } = "Pippo!";
-        public bool IsDone { get; set; }
-    }
+    public int Id { get; set; }
+    public string? Titolo { get; set; }
+    public bool IsDone { get; set; }
+    public DateOnly InseritoIl { get; private set; }
+        = DateOnly.FromDateTime(DateTime.Now);
+    public DateTime UltimaModifica { get; set; }= DateTime.Now;
 }
