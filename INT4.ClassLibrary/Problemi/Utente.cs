6970bcd6f0798df131572ade8af63ed4df22fa0c
Davide Primino <dprimino@olivelliputelli.it>
2025-01-09 10:32:24 +0100

    Problemi

---
+++ INT4.ClassLibrary/Problemi/Utente.cs
@@ -0,0 +1,18 @@
+﻿namespace INT4.ClassLibrary.Problemi;
+public class Utente
+{
+    public string Nome { get; set; } = String.Empty;
+    public string Cognome { get; set; } = String.Empty;
+    public DateOnly DataDiNascita { get; set; }
+    public int Eta()
+    {
+        int eta = DateTime.Now.Year - DataDiNascita.Year;
+        if (DateOnly.FromDateTime(DateTime.Now.AddYears(-eta)) < DataDiNascita) eta--;
+        return eta;
+    }
+    public TimeSpan TempoProssimoCompleanno()
+    {
+        
+        return DateTime.Now - DataDiNascita.AddYears(Eta()).ToDateTime(new TimeOnly());
+    }
+}
